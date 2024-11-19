101690e53da1581ed37f933c04a750cd31b56927
Fernandodennis <fernandodennis038@gmail.com>
2024-11-19 22:09:47 +0700

    need to fetch data from database, and replace with actual data from model/database in app/Http/Controllers/supliercontroller.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
-<?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SuplierController;
 
 Route::get('/', function () {
     return view('home');
 });
+
+// Route for suplier.index
+Route::get('/supliers', [SuplierController::class, 'index'])->name('suplier.index');
